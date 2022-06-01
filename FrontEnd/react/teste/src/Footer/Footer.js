2895ebff6ba863962bad7a8476a4061779c72c6d
delberss <delberss@hotmail.com>
2022-06-01 14:42:42 -0300

    Criacao do header, footer e rotas

---
+++ FrontEnd/react/teste/src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import '../Footer/styles.css'
+
+function Footer(){
+    return(
+        <div className="footer">
+            <p>© 2022 Delber_Kennedy_Nicolas_Wesley</p>
+        </div>
+    )
+}
+
+export default Footer;
