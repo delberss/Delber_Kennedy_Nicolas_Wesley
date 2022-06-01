a969548657cf5c08ab02a4d28e6c7ac7140da040
delberss <delberss@hotmail.com>
2022-06-01 15:18:40 -0300

    Refatoracao do ranking com ligacao com o banco

--- FrontEnd/react/teste/src/App.js
+++ FrontEnd/react/teste/src/App.js
@@ -1,38 +1,18 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+
+import Rotas from './Routes/Rotas';
+
+
 
 function App() {
 
-  const [resu,setResu] =useState([]);
-
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://trabalhoengsw.herokuapp.com/usuarios/get/all?pword=senha123", requestOptions)
-    .then(response => response.text())
-    .then((result) => setResu(result))
-    .catch(error => console.log('error', error));
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          usuarios cadastrados : {resu}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <Rotas />
+
     </div>
+
   );
 }
 
--- FrontEnd/react/teste/src/pages/Ranking/index.js
+++ FrontEnd/react/teste/src/pages/Ranking/index.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../../Header/Header";
 import Footer from '../../Footer/Footer';
-import '../../Content/styles.css'
 
+class Ranking extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuarios: []
+        }
+    }
 
-function Ranking(){
-    return(
-        <div>
-            <Header/>
+    componentDidMount() {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            mode: 'cors'
+        };
+
+        fetch("https://trabalhoengsw.herokuapp.com/usuarios/ranking/get?pword=senha123", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                this.setState({ usuarios: JSON.parse(result) })
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    usuariosJsonToList() {
+        const userList = [];
+        for (let i = 0; i < this.state.usuarios.length; i++) {
+            let nome = this.state.usuarios[i]['nome'];
+            let xp = this.state.usuarios[i]['experiencia'];
+            userList.push(<li key={i}>Nome = {nome}, xp = {xp}</li>);
+        }
+        return userList;
+    }
+
+    render() {
+
+        return (<div>
+            <Header />
             <div className="content">
-                <h2>RANKING</h2>
+                <div>
+                    <h2>RANKING</h2>
+                </div>
+
+
+                <ul>  {this.usuariosJsonToList()}</ul>
+
             </div>
-            <Footer/>
+            <Footer />
         </div>
-        
-    )
+        );
+    }
 }
+
 
 export default Ranking;
