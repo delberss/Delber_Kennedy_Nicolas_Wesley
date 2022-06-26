f3e6588fc55b15dc5997b784d6cd21bee60299b4
kennedy <kennedycvieira@gmail.com>
2022-06-26 03:25:44 -0300

    Melhorias nas tabelas

--- src/createDb.js
+++ src/createDb.js
@@ -7,22 +7,28 @@ var router = express.Router();
 router.get('/', async (req, res) => {
   if (req.query.pword == "senha123") {
     const CREATE_TABLE =
-    'CREATE TABLE usuarios (id SERIAL PRIMARY KEY, nome VARCHAR, email VARCHAR UNIQUE, curso VARCHAR'+
-    'senha VARCHAR NOT NULL, experiencia JSONB, caminhoAtual INTEGER,caminhoBack JSONB,caminhoFront JSONB);'+
-    'CREATE TABLE caminhos (id SERIAL PRIMARY KEY, nome VARCHAR, descricao VARCHAR, numeroTopicos INTEGER);'+
-    'CREATE TABLE topicos (id SERIAL PRIMARY KEY, nome varchar, descricao VARCHAR);'+
-    'CREATE TABLE subtopicos (id SERIAL PRIMARY KEY, nome VARCHAR, descricao VARCHAR, conteudo JSONB);'+
-    'CREATE TABLE caminhoContemTopico(id SERIAL , caminho INTEGER REFERENCES caminhos ON DELETE CASCADE,'+
-    'topico INTEGER REFERENCES topicos ON DELETE CASCADE, PRIMARY KEY (caminho,topico));'+
-    'CREATE TABLE topicoContemSubtopico(id SERIAL , topico INTEGER REFERENCES topicos ON DELETE CASCADE,'+
-    'subtopico INTEGER REFERENCES subtopicos ON DELETE CASCADE, PRIMARY KEY (topico,subtopico));'+
+    'CREATE TABLE usuarios (id SERIAL PRIMARY KEY, nome VARCHAR NOT NULL, email VARCHAR UNIQUE, curso VARCHAR'+
+    'senha VARCHAR NOT NULL, experiencia INTEGER, caminhoAtual INTEGER,caminhoBack JSONB,caminhoFront JSONB);'+
+    'CREATE TABLE caminhos (id SERIAL PRIMARY KEY, nome VARCHAR, descricao VARCHAR);'+
+    'CREATE TABLE topicos (id SERIAL PRIMARY KEY, nome varchar NOT NULL, descricao VARCHAR);'+
+    'CREATE TABLE subtopicos (id SERIAL PRIMARY KEY, nome VARCHAR NOT NULL, descricao VARCHAR);'+
+    'CREATE TABLE conteudos (id SERIAL PRIMARY KEY,tipo VARCHAR NOT NULL, link VARCHAR);'+
+    'CREATE TABLE caminhoContemTopico(id SERIAL PRIMARY KEY , caminho INTEGER REFERENCES caminhos ON DELETE CASCADE,'+
+    'topico INTEGER REFERENCES topicos ON DELETE CASCADE, UNIQUE (caminho,topico));'+
+    'CREATE TABLE topicoContemSubtopico(id SERIAL PRIMARY KEY , topico INTEGER REFERENCES topicos ON DELETE CASCADE,'+
+    'subtopico INTEGER REFERENCES subtopicos ON DELETE CASCADE, UNIQUE (topico,subtopico));'+
     'CREATE TABLE feedbackSubtopico(id SERIAL PRIMARY KEY, usuario INTEGER REFERENCES usuarios ON DELETE CASCADE,'+ 
     'subtopico INTEGER REFERENCES subtopicos ON DELETE CASCADE,  criadoEm TIMESTAMP, editadoEm TIMESTAMP, conteudo VARCHAR) ;'+
     'CREATE TABLE feedbackTopico(id SERIAL PRIMARY KEY, usuario INTEGER REFERENCES usuarios ON DELETE CASCADE,'+ 
     'topico INTEGER REFERENCES topicos ON DELETE CASCADE , criadoEm TIMESTAMP, editadoEm TIMESTAMP, conteudo VARCHAR);'
     'CREATE TABLE feedbackCaminho(id SERIAL PRIMARY KEY, usuario INTEGER REFERENCES usuarios ON DELETE CASCADE, '+
-    'caminho INTEGER REFERENCES caminhos ON DELETE CASCADE, criadoEm TIMESTAMP, editadoEm TIMESTAMP, conteudo VARCHAR);';
-    
+    'caminho INTEGER REFERENCES caminhos ON DELETE CASCADE, criadoEm TIMESTAMP, editadoEm TIMESTAMP, conteudo VARCHAR);'+
+    'CREATE TABLE caminhoContemConteudo (id SERIAL PRIMARY KEY,caminho INTEGER REFERENCES caminhos,'+
+    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(caminho,conteudo));'+
+    'CREATE TABLE topicoContemConteudo (id SERIAL PRIMARY KEY,topico INTEGER REFERENCES topicos,'+
+    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(caminho,conteudo));'+
+    'CREATE TABLE subtopicoContemConteudo (id SERIAL PRIMARY KEY,subtopico INTEGER REFERENCES subtopicos,'+
+    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(caminho,conteudo));';
 
     try {
       const client = await pool.connect();
