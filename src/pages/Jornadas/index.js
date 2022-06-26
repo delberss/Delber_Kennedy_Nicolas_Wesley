f55e2eb014ecfe5757ffb9a67d164d34d42eaf7f
kennedy <kennedycvieira@gmail.com>
2022-06-26 20:20:24 -0300

    import

--- src/pages/Jornadas/index.js
+++ src/pages/Jornadas/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import '../Jornadas/style.css'
 import Header from "../../Header/Header";
 import Footer from '../../Footer/Footer';
