3e80f7d0005ce43914d16c1024883af3ad252361
delberss <71342302+delberss@users.noreply.github.com>
2022-06-26 22:37:55 -0300

    Update index.js

--- src/pages/Jornadas/index.js
+++ src/pages/Jornadas/index.js
@@ -5,60 +5,6 @@ import Footer from '../../Footer/Footer';
 import '../../Content/styles.css'
 
 class Jornadas extends Component {
-<<<<<<< HEAD
-=======
-    constructor(props) {
-        super(props);
-        this.state = {
-            frontend: [],
-            backend: []
-        }
-    }
-
-    componentDidMount() {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-            mode: 'cors'
-        };
-
-        fetch("https://trabalhoengsw.herokuapp.com/caminhos/get/topicos?idCaminho=1", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                this.setState({ frontend: JSON.parse(result) })
-            })
-            .catch(error => console.log('error', error));
-
-            fetch("https://trabalhoengsw.herokuapp.com/caminhos/get/topicos?idCaminho=2", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                this.setState({ backend: JSON.parse(result) })
-            })
-            .catch(error => console.log('error', error));
-    }
-
-    frontJsonToList() {
-        const frontList = [];
-        for (let i = 0; i < this.state.frontend.length; i++) {
-            let id = this.state.frontend[i]['id'];
-            let nome = this.state.frontend[i]['nome'];
-            let descricao = this.state.frontend[i]['descricao'];
-            frontList.push(<li key={id}>Nome = {nome}, descricao = {descricao}</li>);
-        }
-        return frontList;
-    }
-    
-    backJsonToList() {
-        const backList = [];
-        for (let i = 0; i < this.state.backend.length; i++) {
-            let id = this.state.backend[i]['id'];
-            let nome = this.state.backend[i]['nome'];
-            let descricao = this.state.backend[i]['descricao'];
-            backList.push(<li key={id}>Nome = {nome}, descricao = {descricao}</li>);
-        }
-        return backList;
-    }
->>>>>>> 78121e62e3fcbaebfd60a22d4b34d09173b56581
 
     render() {
         return(
@@ -67,7 +13,6 @@ class Jornadas extends Component {
                 <div className="content">
                     <h2 className="jornadasBackFront">Jornadas</h2>
                     <div className="jornadas">
-<<<<<<< HEAD
                         <a className="box-backend" href="/jornadaback">
                             <strong>Backend</strong>
                         </a>
@@ -75,21 +20,6 @@ class Jornadas extends Component {
                         <a className="box-frontend" href="/jornadafront">
                             <strong>Frontend</strong>
                         </a>
-=======
-                        <div className="backend">
-                            <h2>Back-end</h2>
-                            <ul>
-                            {this.frontJsonToList()}
-                            </ul>
-                        </div>
-    
-                        <div className="frontend">
-                            <h2>Front-end</h2>
-                            <ul>
-                            {this.backJsonToList()}
-                            </ul>
-                        </div>
->>>>>>> 78121e62e3fcbaebfd60a22d4b34d09173b56581
                         
                     </div>
                 </div>
@@ -103,4 +33,3 @@ class Jornadas extends Component {
 
 
 export default Jornadas;
-
