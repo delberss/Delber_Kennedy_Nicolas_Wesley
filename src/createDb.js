6c4274fd95047773ab2311e07cb290a73b292cd2
kennedy <kennedycvieira@gmail.com>
2022-07-22 19:56:21 -0300

    sql

--- src/createDb.js
+++ src/createDb.js
@@ -47,12 +47,12 @@ router.get('/', async (req, res) => {
 router.get('/custom', async (req, res) => {
 
   const CREATE_TABLE =
-    'CREATE TABLE IF NOT EXISTS caminhoContemConteudo (id SERIAL PRIMARY KEY,caminho INTEGER REFERENCES caminhos,' +
-    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(caminho,conteudo));' +
-    'CREATE TABLE IF NOT EXISTS topicoContemConteudo (id SERIAL PRIMARY KEY,topico INTEGER REFERENCES topicos,' +
-    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(topico,conteudo));' +
-    'CREATE TABLE IF NOT EXISTS subtopicoContemConteudo (id SERIAL PRIMARY KEY,subtopico INTEGER REFERENCES subtopicos,' +
-    ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(subtopico,conteudo));';
+  'CREATE TABLE IF NOT EXISTS caminhoContemConteudo (id SERIAL PRIMARY KEY,caminho INTEGER REFERENCES caminhos,' +
+  ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(caminho,conteudo));' +
+  'CREATE TABLE IF NOT EXISTS topicoContemConteudo (id SERIAL PRIMARY KEY,topico INTEGER REFERENCES topicos,' +
+  ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(topico,conteudo));' +
+  'CREATE TABLE IF NOT EXISTS subtopicoContemConteudo (id SERIAL PRIMARY KEY,subtopico INTEGER REFERENCES subtopicos,' +
+  ' conteudo INTEGER REFERENCES conteudos ON DELETE CASCADE, UNIQUE(subtopico,conteudo));';
 
   try {
     const client = await pool.connect();
